ac80a3eed0fcef6fd39082a98fc8758b786a31f7
Catz (CodingCat) <103620558+CodingCatz@users.noreply.github.com>
2026-05-18 21:39:25 +0800

    Class16:打擊震動效果

--- Assets/Scripts/Skill/SkillCtrl.cs
+++ Assets/Scripts/Skill/SkillCtrl.cs
@@ -1,12 +1,39 @@
 ﻿using UnityEngine;
+using Unity.Cinemachine;
 
 public class SkillCtrl : MonoBehaviour
 {
+    #region 基礎元建
+    /// <summary>
+    /// 鏡頭震動元件本體
+    /// </summary>
+    private CinemachineImpulseSource _impulseSource;
+    private CinemachineImpulseSource impulseSource
+    {
+        get 
+        { 
+            if (_impulseSource == null)
+            {
+                _impulseSource = GetComponent<CinemachineImpulseSource>();
+                if (_impulseSource == null )
+                    _impulseSource = gameObject.AddComponent<CinemachineImpulseSource>();
+            }
+            return _impulseSource; 
+        }
+    }
+
+    #endregion 基礎元建
+
     public enum Target { None, Enemy, Player }
     [SerializeField]
     private Target target;
     [SerializeField]
     private GameObject _hitEffectObj;
+    [SerializeField]
+    private float _hitPower = 0f;
+    private bool HitShock => _hitPower > 0f;
+    [SerializeField]
+    private float _destroyTime = 2f;
     private string Tag
     {
         get
@@ -22,7 +49,7 @@ public class SkillCtrl : MonoBehaviour
     
     void Start()
     {
-        
+        Destroy(gameObject, _destroyTime);
     }
 
     /// <summary>
@@ -34,6 +61,7 @@ public class SkillCtrl : MonoBehaviour
         if (other.tag == Tag)
         {
             _hitEffectObj.SetActive(true);
+            if (HitShock) impulseSource.GenerateImpulseWithForce(_hitPower);
         }
     }
 }

a6719cde249d46852b966d0f01f1d6978d4e0381
Catz (CodingCat) <103620558+CodingCatz@users.noreply.github.com>
2026-05-27 21:44:20 +0800

    Class20:關卡設計(環境效果與鏡頭)

---
+++ Assets/Scripts/Controller/RoomCtrl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Unity.Cinemachine;
+
+public class RoomCtrl : MonoBehaviour
+{
+    #region 基礎元建
+    [SerializeField]
+    private CinemachineCamera cinemachineCamera;
+
+    #endregion 基礎元建
+    private const string Tag = "Player";
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(Tag))
+        {
+            cinemachineCamera.PreviousStateIsValid = true;
+        }
+    }
+}
--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -57,32 +57,22 @@ public static class GameManager
     }
     #endregion 玩家相關資訊
 
-    #region 攝影機相關資訊
-    /// <summary>
-    /// 當前運作中攝影機
-    /// </summary>
-    public static CameraCtrl cameraCtrl { get; private set; }
-    /// <summary>
-    /// 當前運作中攝影機的旋轉參數
-    /// </summary>
-    public static Vector3 cameraRota
+    #region 主攝影機相關
+    public static CameraManager mainCamrea { get; private set; }
+    public static Vector3 mainCameraRota
     {
         get
         {
-            return cameraCtrl != null
-                ? cameraCtrl.transform.rotation.eulerAngles
+            return mainCamrea != null
+                ? mainCamrea.transform.rotation.eulerAngles
                 : Vector3.zero;
         }
     }
-    /// <summary>
-    /// 設定(初始化)當前操作鏡頭
-    /// </summary>
-    /// <param name="ctrl">鏡頭控制器</param>
-    public static void SetCurrentCamera(CameraCtrl ctrl)
+    public static void SetMainCamera(CameraManager main)
     {
-        cameraCtrl = ctrl;
+        mainCamrea = main;
     }
-    #endregion 攝影機相關資訊
+    #endregion 主攝影機相關
 
     public static void LoadScene(string sceneName)
     {
